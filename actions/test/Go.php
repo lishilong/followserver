78e4a48ff9cc34aa83b7f547eb098d5741488e90
Peter <houpeng01@baidu.com>
2017-10-23 20:49:54 +0800

    demo

--- actions/test/Go.php
+++ actions/test/Go.php
@@ -12,6 +12,38 @@ class Action_Go extends Ap_Action_Abstract {
         $request = $this->getRequest();
         $requestParams = $request->getRequest();
         
+        // db连接
+        $db = Box_Db_ConnMgr::getConn('boxlib_media');
+        $db->query($sql);
+        
+        // meta获取
+        $data  = array(
+            array('nid'=>'dt_', 'field'=> ''),
+            array('nid'=>'dt_', 'field'=> ''),
+            array('nid'=>'dt_', 'field'=> ''),
+        );
+        $ret = Box_Feedmeta_Cache::mgetCache($data);
+        
+        // http请求
+        $reqData = array(
+            'service' => 'bdbox_ugc_bjh',
+            'method' => 'post',
+            'input' => $tempData,
+            'header' => array(
+                'pathinfo'      => '/builder/author/dynamic/create',
+            ),
+        );
+        $ralObj = new Utils_Ral_SingleHttp($reqData);
+        $response = $ralObj->request();
+        
+        // redis获取
+        $config = array(
+            'pid' => 'mbubsweb',
+            'tk'  => 'mbubsweb',
+            'app' => 'integral',
+        );
+        $redis = Mbd_Redis_Base::factory($config);
+        
     }
 
 }
