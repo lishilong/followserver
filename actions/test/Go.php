971279d50f72637f80b2b7f35e2fef31801e14a1
Peter <houpeng01@baidu.com>
2017-10-23 11:14:10 +0800

    test

---
+++ actions/test/Go.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author houpeng <houpeng01@baidu.com>
+ */
+class Action_Go extends Ap_Action_Abstract {
+
+    protected $maps;
+    protected $data;
+
+    public function execute() {
+        $request = $this->getRequest();
+        $requestParams = $request->getRequest();
+        
+    }
+
+}
---
+++ controllers/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @author houpeng <houpeng01@baidu.com>
+ */
+class Controller_Test extends Ap_Controller_Abstract {
+
+    public $actions = array(
+        'go' => 'actions/test/Go.php',
+    );
+
+}
