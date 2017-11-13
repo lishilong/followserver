fb8f8f20a43cd0856f3fe58ea3353d4ed1dd1bcd
v_lishilong <v_lishilong@baidu.com>
2017-11-13 12:47:51 +0800

    update read
    Change-Id: I16ba25f6b095c7631e58718f0102ed21c37f6beb

--- models/service/data/Read.php
+++ models/service/data/Read.php
@@ -22,16 +22,63 @@ class Service_Data_Read extends Service_Data_Base
         }
 
         //拼装数组
-        $newArrayNids = $this->addArrayPrefix($arrayNids);
+        $newArrayNids = $this->_multiAddDtPrefix($arrayNids);
 
         $result = Box_Social_Celebrity_InteractiveData::mgetViewCount($newArrayNids);
 
         if(is_array($result) &&  !empty($result['mget']))
         {
              //删除数组KEY的前缀
-              $arr = $this->delArrayPrefix($result['mget']);
+              $arr = $this->_multiRemoveDtPrefix($result['mget']);
         }
 
         return $arr;
       }
+
+    /**
+     * 拼装数组前缀dt_
+     * @param $arrayNids
+     * @return array
+     */
+    private function _multiAddDtPrefix($arrayNids)
+    {
+        $list = array();
+
+        foreach($arrayNids as $val){
+            $str = substr($val,0,3);
+            if($str == 'dt_'){
+                $list[] = $val;
+            }else{
+                $list[] = 'dt_'.$val;
+            }
+        }
+
+        return $list;
+    }
+
+
+    /**
+     * 去除数组KEY前缀dt_
+     * @param $arrayMget
+     * @return array
+     */
+    private function _multiRemoveDtPrefix($arrayMget)
+    {
+        $data = array();
+
+        foreach($arrayMget as $key => $val)
+        {
+            $str = substr($key,0,3);
+            if($str == "dt_")
+            {
+                $newKey = substr($key,3);
+                $data[$newKey] = $val;
+            }else{
+                $data[$key] = $val;
+            }
+        }
+
+        return $data;
+    }
+
 }
